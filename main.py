a1474c8858feaa9502a23a23ca968307b3ebe85b
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-12 23:38:58 +0900

    Change background and other

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class App(Frame):
 	def __init__(self,master=None):
 		super().__init__(master)
 		self.pack()
+		self.config(bg="white")
 		self.all_here()
 
 	def all_here(self):
@@ -21,23 +22,27 @@ class App(Frame):
 		self.info = Toplevel()
 		self.info.geometry("240x100")
 
-		self.m = Message(self.info, text="This is program built with Tk 8.5 and Random Choice module.", fg="blue")
+		self.m = Message(self.info, text="This is program built with Tk 8.5 and Random Choice module.", fg="blue", bg="white")
 		self.m.pack()
 
 		self.dismiss = Button(self.info, text="DISMISS", bg="red", fg="white", command=self.info.destroy)
 		self.dismiss.pack()
+
+		self.info.config(bg="white")
 		# end of this is info for project
 
 		# all here user need
 		self.heading = Label(self)
 		self.heading["text"] = "Random JM"
+		self.heading["bg"] = "white"
 		self.heading["fg"] = "blue"
 		self.heading["font"] = ("Cooper Black",30)
 		self.heading.pack(pady=20)
 
 		self.info_here = Label(self)
 		self.info_here["text"] = "\"your lucky is good to choice right number in application.\"\n\nnumber start by 0 to 10!"
-		self.info_here["font"] = ("Times New Roman Italic",12)
+		self.info_here["bg"] = "white"
+		self.info_here["font"] = ("Times New Roman Italic",14)
 		self.info_here.pack(pady=5)
 
 		# promt to user insert a number
@@ -46,7 +51,7 @@ class App(Frame):
 		# end of prompt to user insert a number
 
 		# button here
-		self.button_frame = Frame(self)
+		self.button_frame = Frame(self, bg="white")
 		self.button_frame.pack(pady=8)
 
 		self.b1 = Button(self.button_frame, text="Insert", bg="green", fg="white", command=self.tested_number)
@@ -58,6 +63,7 @@ class App(Frame):
 
 		self.result = Label(self)
 		self.result["text"] = "Info Here!"
+		self.result["bg"] = "white"
 		self.result["font"] = ("Fira Code Regular",12)
 		self.result.pack(pady=20)
 
@@ -68,18 +74,19 @@ class App(Frame):
 			a = [0,1,2,3,4,5,6,7,8,9,10]
 			your_number = int(self.user_number.get())
 			if( your_number == choice(a) ):
-				print("Good Choice!")
+				self.result.config(text="You Won. you very lucky today!", fg="blue")
 			elif( your_number > choice(a) ):
-				print("Your number is a big!")
+				self.result.config(text="your number is a BIG!", fg="red")
 			elif( your_number < choice(a) ):
-				print("your number is a small!")
+				self.result.config(text="your number is a SMALL!", fg="blue")
 			else:
-				print("not a number!")
+				self.result.config(text="This is not a number!", fg="white", bg="red")
 		except Exception as err:
 			print("error :", err)
 
 	def deleted(self):
 		self.user_number.delete(0, END)
+		self.result.config(text="Info Here!", fg="black", bg="white")
 
 root = Tk()
 root.iconbitmap("icon.ico")
@@ -88,4 +95,5 @@ root.geometry("600x400")
 
 app = App(master=root)
 
+root.config(bg="white")
 root.mainloop()
